64c6be83d069f65625a0a05a982a6d0ca53b4bdd
0xdagarn <sgnoo@naver.com>
2024-09-11 10:21:00 +0900

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,21 @@ export const metadata = {
   title: "Ycle",
   description:
     "Ycle is a platform for recording cycles and connecting with others.",
+  openGraph: {
+    title: "Ycle - Record Your Cycles",
+    description: "Connect and share your cycles with others on Ycle",
+    url: "https://why-mvn.vercel.app",
+    siteName: "Ycle",
+    images: [
+      {
+        url: "https://why-mvn.vercel.app/logo.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
