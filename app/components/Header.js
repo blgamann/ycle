62d29913b616084ecb21454ba57c5c3a538aaa50
0xdagarn <sgnoo@naver.com>
2024-09-15 14:42:42 +0900

    Refactor header

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,32 +5,42 @@ import { Button } from "@/components/ui/button";
 export function Header({ isLoggedIn, onLogout }) {
   return (
     <header>
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 mt-8">
-        <div className="flex justify-between items-center h-20">
-          <Link href="/">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              width={130}
-              height={130}
-              className="object-contain"
-            />
-          </Link>
-          {isLoggedIn && (
-            <div>
-              {/* <Link
-                href="/leaderboard"
-                className="text-gray-600 hover:text-gray-900 mr-6"
-              >
-                Leaderboard
-              </Link> */}
-              <Button onClick={onLogout} variant="outline" size="sm">
-                로그아웃
-              </Button>
-            </div>
-          )}
-        </div>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-8">
+        <nav className="flex justify-between items-center h-20">
+          <Logo />
+          {isLoggedIn && <UserMenu onLogout={onLogout} />}
+        </nav>
       </div>
     </header>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <Image
+        src="/logo.png"
+        alt="Logo"
+        width={130}
+        height={130}
+        className="object-contain"
+      />
+    </Link>
+  );
+}
+
+function UserMenu({ onLogout }) {
+  return (
+    <div>
+      {/* <Link
+        href="/leaderboard"
+        className="text-gray-600 hover:text-gray-900 mr-6"
+      >
+        Leaderboard
+      </Link> */}
+      <Button onClick={onLogout} variant="outline" size="sm">
+        로그아웃
+      </Button>
+    </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-screen bg-gray-50">
-        <div className="flex flex-col min-h-screen">{children}</div>
+        <div className="flex flex-col min-h-screen w-full max-w-3xl mx-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
